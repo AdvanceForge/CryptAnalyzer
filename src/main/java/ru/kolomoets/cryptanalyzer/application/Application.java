0265ffdfddfc2d7ec9d54c742ef60233c749378b
Stas Kolomoets <stas31017@yandex.ru>
2025-04-27 20:21:05 +0300

    created the structure of the project

---
+++ src/main/java/ru/kolomoets/cryptanalyzer/application/Application.java
@@ -0,0 +1,14 @@
+package ru.kolomoets.cryptanalyzer.application;
+
+
+import ru.kolomoets.cryptanalyzer.controller.MainController;
+
+public class Application {
+
+    public static void main( String[] args ) {
+
+        MainController controller = new MainController();
+        controller.run();
+
+    }
+}
---
+++ src/main/java/ru/kolomoets/cryptanalyzer/core/CaesarCipher.java
@@ -0,0 +1,9 @@
+package ru.kolomoets.cryptanalyzer.core;
+
+public class CaesarCipher {
+
+//    public String encrypt(String text, int key){}
+//
+//    public String decrypt(String text, int key){}
+
+}
